9fffc159db15da92b9a2c38d38f81c8212dc8383
inmyblue <inmyblue0930@naver.com>
2022-04-01 20:01:42 +0900

    순한맛완료

--- src/index.js
+++ src/index.js
@@ -1,10 +1,66 @@
-class Site {}
+class Site {
+    constructor(){
+        this.boards = []
+    }
 
-class Board {}
+    addBoard(board){
+        if(this.boards.find(v=>v.name === board.name)) throw new Error("Error")
+        board.addBoardTF = true
+        this.boards.push(board)
+    }
+    
+    findBoardByName(board){
+        return this.boards.find(v => v.name === board)
+    }
+}
 
-class Article {}
+class Board{
+    constructor(name){
+        if(!name) throw new Error("Error")
+        this.name = name
+        this.addBoardTF = false
+        this.articles = []
+    }
 
-class Comment {}
+    publish(article){
+        if(this.addBoardTF === false) throw new Error("Error")
+        article.id = this.name+'-'+Math.random()
+        article.createdDate = new Date().toISOString()
+        article.addArticleTF=true
+        this.articles.push(article)
+    }
+
+    getAllArticles(){
+        return this.articles
+    }
+}
+
+class Article {
+    constructor(article){
+        const {subject, content, author} = article
+        if(!subject || !content || !author) throw new Error("Error")
+
+        this.addArticleTF = false
+        this.comments = []
+    }
+
+    reply(comment){
+        if(this.addArticleTF == false) throw new Error("Error")
+        comment.createdDate = new Date().toISOString()
+        this.comments.push(comment)
+    }
+
+    getAllComments(){
+        return this.comments
+    }
+}
+
+class Comment {
+    constructor(comment){
+        const {content, author} = comment
+        if(!content || !author) throw new Error("Error")
+    }
+}
 
 module.exports = {
     Site,
